14519de028196ca2dbe038f3df6faa52d475c8ed
Daniel Erat <dan@erat.org>
2020-12-25 13:15:10 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# ytskip
+
+**ytskip** is a Chrome [content script extension] that automatically clicks
+buttons on YouTube pages.
+
+[content script extension]: https://developer.chrome.com/extensions/content_scripts
---
+++ content.js
@@ -0,0 +1,84 @@
+// Copyright 2020 Daniel Erat. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Minimum duration between evaluating the page state due to DOM mutations.
+const mutationDebounceMs = 100;
+
+// Clicks things.
+class Clicker {
+  constructor() {
+    this.lastCheckTime = 0;
+    this.checkTimeoutId = undefined;
+
+    this.player = undefined;
+    this.playerObserver = new MutationObserver(m => this.onPlayerMutation());
+
+    // Observe the main ytd-app element for page changes.
+    this.app = document.querySelector('ytd-app');
+    if (!this.app) throw new Error('Failed to find ytd-app element');
+    this.appObserver = new MutationObserver(m => this.onAppMutation());
+    this.appObserver.observe(this.app, {
+      attributes: true,
+      atributeFilter: ['is-watch-page'],
+    });
+
+    // Handle a watch page being loaded directly.
+    this.onAppMutation();
+  }
+
+  // Handles mutations to the ytd-app element (used to detect navigation to or
+  // from a /watch page).
+  onAppMutation() {
+    const player = document.querySelector('ytd-player');
+    if (player && this.player === player) return; // already watching player
+
+    this.player = player;
+    this.playerObserver.disconnect();
+    if (this.checkTimeoutId !== undefined) {
+      window.clearTimeout(this.checkTimeoutId);
+      this.checkTimeoutId = undefined;
+    }
+
+    if (this.player) {
+      console.log('Observing player for changes');
+      this.playerObserver.observe(this.player, {
+        childList: true,
+        subtree: true,
+      });
+    }
+  }
+
+  // Handles mutations to the ytd-player element (used to detect buttons being
+  // added).
+  onPlayerMutation() {
+    if (this.checkTimeoutId !== undefined) return; // already queued
+
+    const elapsed = new Date().getTime() - this.lastCheckTime;
+    if (elapsed >= mutationDebounceMs) {
+      this.check();
+    } else {
+      this.checkTimeoutId = window.setTimeout(() => {
+        this.checkTimeoutId = undefined;
+        this.check();
+      }, mutationDebounceMs - elapsed);
+    }
+  }
+
+  // Checks for skip/close buttons.
+  check() {
+    this.click('.ytp-ad-overlay-close-button');
+    this.click('.ytp-ad-skip-button');
+    this.lastCheckTime = new Date().getTime();
+  }
+
+  // Clicks the element matched by |sel| (if any).
+  click(sel) {
+    const el = this.player.querySelector(sel);
+    if (!el) return;
+    console.log(`Clicking ${sel}`);
+    el.click();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => new Clicker());
